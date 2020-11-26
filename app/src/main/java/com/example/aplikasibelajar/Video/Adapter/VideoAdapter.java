34f1f32dec2c4bf1eb6acda18c81fb17fc9aabca
fadilHidayatul <bangtans46@gmail.com>
2020-11-26 16:59:21 +0700

    Youtube Player to Activity

--- app/src/main/java/com/example/aplikasibelajar/Video/Adapter/VideoAdapter.java
+++ app/src/main/java/com/example/aplikasibelajar/Video/Adapter/VideoAdapter.java
@@ -1,15 +1,18 @@
 package com.example.aplikasibelajar.Video.Adapter;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.aplikasibelajar.R;
+import com.example.aplikasibelajar.Video.PlayVideoActivity;
 import com.example.aplikasibelajar.Video.Video;
 
 import java.util.List;
@@ -36,6 +39,14 @@ public class VideoAdapter extends RecyclerView.Adapter<VideoAdapter.viewholder> 
     @Override
     public void onBindViewHolder(@NonNull VideoAdapter.viewholder holder, int position) {
         holder.namaVideo.setText(dataBeanList.get(position).getJudul_video());
+        holder.cardSelectVideo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(context, PlayVideoActivity.class);
+                intent.putExtra("id", dataBeanList.get(position).getLink());
+                context.startActivity(intent);
+            }
+        });
     }
 
     @Override
@@ -46,6 +57,8 @@ public class VideoAdapter extends RecyclerView.Adapter<VideoAdapter.viewholder> 
     public class viewholder extends RecyclerView.ViewHolder {
         @BindView(R.id.namaVideo)
         TextView namaVideo;
+        @BindView(R.id.cardSelectVideo)
+        CardView cardSelectVideo;
         public viewholder(@NonNull View itemView) {
             super(itemView);
             ButterKnife.bind(this,itemView);
---
+++ app/src/main/java/com/example/aplikasibelajar/Video/PlayVideoActivity.java
@@ -0,0 +1,118 @@
+package com.example.aplikasibelajar.Video;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.LifecycleObserver;
+
+import com.example.aplikasibelajar.R;
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.PlayerConstants;
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.YouTubePlayer;
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.listeners.AbstractYouTubePlayerListener;
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.views.YouTubePlayerView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class PlayVideoActivity extends AppCompatActivity implements LifecycleObserver {
+
+    @BindView(R.id.playerView)
+    YouTubePlayerView playerView;
+
+    Context context;
+    @BindView(R.id.btnBack)
+    ImageView btnBack;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_play_video);
+        ButterKnife.bind(this);
+        context = this;
+
+        back();
+        Intent intent = getIntent();
+        String idVideo = intent.getStringExtra("id");
+        getLifecycle().addObserver(playerView);
+
+        playerView.addYouTubePlayerListener(new AbstractYouTubePlayerListener() {
+            @Override
+            public void onReady(YouTubePlayer youTubePlayer) {
+                youTubePlayer.cueVideo(idVideo, 0);
+            }
+
+            @Override
+            public void onStateChange(YouTubePlayer youTubePlayer, PlayerConstants.PlayerState state) {
+                super.onStateChange(youTubePlayer, state);
+            }
+
+            @Override
+            public void onApiChange(YouTubePlayer youTubePlayer) {
+                super.onApiChange(youTubePlayer);
+            }
+
+            @Override
+            public void onCurrentSecond(YouTubePlayer youTubePlayer, float second) {
+                super.onCurrentSecond(youTubePlayer, second);
+            }
+
+            @Override
+            public void onError(YouTubePlayer youTubePlayer, PlayerConstants.PlayerError error) {
+                super.onError(youTubePlayer, error);
+            }
+
+            @Override
+            public void onPlaybackQualityChange(YouTubePlayer youTubePlayer, PlayerConstants.PlaybackQuality playbackQuality) {
+                super.onPlaybackQualityChange(youTubePlayer, playbackQuality);
+            }
+
+            @Override
+            public void onPlaybackRateChange(YouTubePlayer youTubePlayer, PlayerConstants.PlaybackRate playbackRate) {
+                super.onPlaybackRateChange(youTubePlayer, playbackRate);
+            }
+
+            @Override
+            public void onVideoDuration(YouTubePlayer youTubePlayer, float duration) {
+                super.onVideoDuration(youTubePlayer, duration);
+            }
+
+            @Override
+            public void onVideoId(YouTubePlayer youTubePlayer, String videoId) {
+                super.onVideoId(youTubePlayer, videoId);
+            }
+
+            @Override
+            public void onVideoLoadedFraction(YouTubePlayer youTubePlayer, float loadedFraction) {
+                super.onVideoLoadedFraction(youTubePlayer, loadedFraction);
+            }
+        });
+        playerView.enterFullScreen();
+        playerView.isFullScreen();
+        playerView.isFullScreen();
+        playerView.toggleFullScreen();
+
+        playerView.getPlayerUiController().setVideoTitle("Best of this month");
+    }
+
+    private void back() {
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                playerView.release();
+                finish();
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        playerView.release();
+        finish();
+    }
+}
