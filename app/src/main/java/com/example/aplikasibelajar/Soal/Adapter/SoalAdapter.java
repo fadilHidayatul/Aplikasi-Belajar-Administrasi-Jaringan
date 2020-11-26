e8bb18e12fdfb1a81ba6946afdfd16b953ed4ab6
fadilHidayatul <bangtans46@gmail.com>
2020-11-26 12:00:43 +0700

    Add Soal Activity

--- app/src/main/java/com/example/aplikasibelajar/Soal/Adapter/SoalAdapter.java
+++ app/src/main/java/com/example/aplikasibelajar/Soal/Adapter/SoalAdapter.java
@@ -1,16 +1,19 @@
 package com.example.aplikasibelajar.Soal.Adapter;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.aplikasibelajar.Materi.Materi;
 import com.example.aplikasibelajar.R;
+import com.example.aplikasibelajar.Soal.SoalLatihanActivity;
 
 import org.w3c.dom.Text;
 
@@ -38,6 +41,14 @@ public class SoalAdapter extends RecyclerView.Adapter<SoalAdapter.viewHolder> {
     @Override
     public void onBindViewHolder(@NonNull SoalAdapter.viewHolder holder, int position) {
         holder.namaSoal.setText(dataBeans.get(position).getJudul_materi());
+        holder.cardSelectSoal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(context, SoalLatihanActivity.class);
+                intent.putExtra("idSoal", dataBeans.get(position).getId_materi());
+                context.startActivity(intent);
+            }
+        });
     }
 
     @Override
@@ -48,6 +59,8 @@ public class SoalAdapter extends RecyclerView.Adapter<SoalAdapter.viewHolder> {
     public class viewHolder extends RecyclerView.ViewHolder {
         @BindView(R.id.namaSoal)
         TextView namaSoal;
+        @BindView(R.id.cardSelectSoal)
+        CardView cardSelectSoal;
         public viewHolder(@NonNull View itemView) {
             super(itemView);
             ButterKnife.bind(this,itemView);
---
+++ app/src/main/java/com/example/aplikasibelajar/Soal/Soal.java
@@ -0,0 +1,136 @@
+package com.example.aplikasibelajar.Soal;
+
+import java.util.List;
+
+public class Soal {
+
+    /**
+     * pesan : soal ada
+     * status : 200
+     * data : [{"id_evaluasi":"3","id_materi":"14","soal":"hadahsfkja\r\n","pil_a":"hadahsfkja\r\n","pil_b":"hadahsfkja\r\n","pil_c":"hadahsfkja\r\n","pil_d":"hadahsfkja\r\n","pil_e":"hadahsfkja\r\n","kunci":"c"},{"id_evaluasi":"4","id_materi":"2","soal":"asd","pil_a":"qweq","pil_b":"asda","pil_c":"qwrrw","pil_d":"asd","pil_e":"zxc","kunci":"e"}]
+     */
+
+    private String pesan;
+    private int status;
+    private List<DataBean> data;
+
+    public String getPesan() {
+        return pesan;
+    }
+
+    public void setPesan(String pesan) {
+        this.pesan = pesan;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public List<DataBean> getData() {
+        return data;
+    }
+
+    public void setData(List<DataBean> data) {
+        this.data = data;
+    }
+
+    public static class DataBean {
+        /**
+         * id_evaluasi : 3
+         * id_materi : 14
+         * soal : hadahsfkja
+         * pil_a : hadahsfkja
+         * pil_b : hadahsfkja
+         * pil_c : hadahsfkja
+         * pil_d : hadahsfkja
+         * pil_e : hadahsfkja
+         * kunci : c
+         */
+
+        private String id_evaluasi;
+        private String id_materi;
+        private String soal;
+        private String pil_a;
+        private String pil_b;
+        private String pil_c;
+        private String pil_d;
+        private String pil_e;
+        private String kunci;
+
+        public String getId_evaluasi() {
+            return id_evaluasi;
+        }
+
+        public void setId_evaluasi(String id_evaluasi) {
+            this.id_evaluasi = id_evaluasi;
+        }
+
+        public String getId_materi() {
+            return id_materi;
+        }
+
+        public void setId_materi(String id_materi) {
+            this.id_materi = id_materi;
+        }
+
+        public String getSoal() {
+            return soal;
+        }
+
+        public void setSoal(String soal) {
+            this.soal = soal;
+        }
+
+        public String getPil_a() {
+            return pil_a;
+        }
+
+        public void setPil_a(String pil_a) {
+            this.pil_a = pil_a;
+        }
+
+        public String getPil_b() {
+            return pil_b;
+        }
+
+        public void setPil_b(String pil_b) {
+            this.pil_b = pil_b;
+        }
+
+        public String getPil_c() {
+            return pil_c;
+        }
+
+        public void setPil_c(String pil_c) {
+            this.pil_c = pil_c;
+        }
+
+        public String getPil_d() {
+            return pil_d;
+        }
+
+        public void setPil_d(String pil_d) {
+            this.pil_d = pil_d;
+        }
+
+        public String getPil_e() {
+            return pil_e;
+        }
+
+        public void setPil_e(String pil_e) {
+            this.pil_e = pil_e;
+        }
+
+        public String getKunci() {
+            return kunci;
+        }
+
+        public void setKunci(String kunci) {
+            this.kunci = kunci;
+        }
+    }
+}
